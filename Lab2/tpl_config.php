18d1ddf2d00cef8d07447563b5e5ecf7af24e46f
Yura-58 <105017229+Yura-58@users.noreply.github.com>
2022-05-09 20:27:01 +0300

    lab2

---
+++ Lab2/tpl_config.php
@@ -0,0 +1,10 @@
+﻿<?php
+require_once(DIR_LIB . 'Twig/Autoloader.php');
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem(DIR_TEMPLATES);
+$twig = new Twig_Environment($loader);
+$twig->addGlobal('site_url', SITE_URL);
+$twig->addGlobal('style', STYLE_URL);
+$template = $twig->loadTemplate($tpl_file);
+$template->display($tpl_vars);
+?>
